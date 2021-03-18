60422a2a7cc3b38a3f0e18ea653e9e2e0600616c
lq <max.li@madv.com>
2021-03-18 14:14:32 +0800

    add golang server

--- Server/README.md
+++ Server/README.md
@@ -8,7 +8,8 @@
 > protoc --php_out=./ --grpc_out=./ --plugin=protoc-gen-grpc=/plugin/grpc_php_plugin hello.proto
 
 - Golang
-> protoc --go_out=./ --plugin=protoc-gen-go=/plugin/grpc_php_plugin hello.proto
+> protoc --go_out=plugins=grpc:. hello.proto
+
 
 ### 说明
 - 使用swoole接收grpc请求，并返回数据。使用swoole而不是golang，是因为我想说明，grpc就是个通信协议。
--- Server/golang/main.go
+++ Server/golang/main.go
@@ -2,74 +2,36 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"net"
-	"io"
-	"lumen-gRPC-demo/grpc/proto"
+
+	pb "../../Proto/services/hello"
+
 	"google.golang.org/grpc"
 )
 
-type HelloService struct{}
+const (
+	port = ":9502"
+)
 
-// SnowflakeService Snowflake gRPC 服务
-type SnowflakeService struct{}
-
-
-// Next 获取下一个唯一 id
-func (s SnowflakeService) Next(ctx context.Context, req *proto.NextRequest) (*proto.NextReply, error) {
-	res := new(proto.NextReply)
-	res.Id = next(req.ServiceId)
-	return res, nil
+type server struct {
+	pb.UnimplementedHelloServer
 }
 
-// 双向流
-func (s SnowflakeService) Hello(stream proto.Snowflake_HelloServer) error {
-	fmt.Println("收到请求")
-	for {
-		in, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-
-		stream.Send(&proto.StreamResp{Message: "Hello " + in.Name})
-
-	}
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", in.GetUsername())
+	return &pb.HelloResponse{Code: 200, Message: "Hello " + in.GetUsername()}, nil
 }
-
-
-func (s SnowflakeService) Test(stream proto.Snowflake_TestServer) error {
-	for {
-		var num int64
-		for i := 0; i < 1000000000; i++ {
-			num++
-		}
-
-		if err := stream.Send(&proto.TestRes{Res: num}); err != nil{
-			return err
-		}
-	}
-}
-
 
 func main() {
-    fmt.Println("初始化")
-	//指定我们期望客户端请求的监听端口。
-	Addr := "127.0.0.1:6666"
-	listen, err := net.Listen("tcp", Addr)
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		fmt.Printf("Failed to listen: %v, program exited\n", err)
-		return
+		log.Fatalf("failed to listen: %v", err)
 	}
-	//创建 gRPC 服务器的一个实例。
-	server := grpc.NewServer()
-
-	//在 gRPC 服务器注册我们的服务实现。
-	var snowflakeService SnowflakeService
-	proto.RegisterSnowflakeServer(server, snowflakeService)
-
-	fmt.Println("Listen on: " + Addr)
-	server.Serve(listen)
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &server{})
+	log.Printf("服务启动" + port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
